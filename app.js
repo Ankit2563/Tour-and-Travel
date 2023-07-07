bc0ffce802a6a335410a4d6a836b173062d86ffc
Ankit2563 <121215269+Ankit2563@users.noreply.github.com>
2023-07-07 16:26:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const ham = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+
+ham.addEventListener("click", function () {
+  ham.classList.toggle("active");
+  menu.classList.toggle("on");
+});
