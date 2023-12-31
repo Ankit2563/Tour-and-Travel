e5e6314dab8c918fc10a90db69518f1f4d6786ef
Ankit Singh Chauhan <ankitcloud2563@gmail.com>
2023-12-31 20:02:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 
 const ham = document.querySelector(".ham");
 const menu = document.querySelector(".menu");
-
+//const new;
 ham.addEventListener("click", function () {
   ham.classList.toggle("active");
   menu.classList.toggle("on");
+  
 });
